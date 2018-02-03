35a9da93a8339ea9d177be84aface18bb0f65182
Hong Yi Chen <hongyigma@gmail.com>
2018-02-03 11:52:16 -0500

    Dabbling around with buttons and HTML

--- mytestscript.js
+++ mytestscript.js
@@ -2,11 +2,15 @@ var results = document.getElementsByClassName("bs-docs-section");
 var temp = document.getElementsByClassName("starter-template");
 for (var i = 0; results.length; i++) {
     //Create the new content to be added
-    var newDiv = document.createElement("div"); 
-    var newContent = document.createTextNode("Hi there and greetings!"); 
-    
+    var newDiv = document.createElement("div");
+    var newContent = document.createTextNode( );
+
+    document.getElementbyID("testBtn").onclick()=function(){
+
+    }
+
     var parentDiv = results[i].parentNode;
-    newDiv.appendChild(newContent);  
-    // add the newly created element and its content into the DOM  
+    newDiv.appendChild(newContent);
+    // add the newly created element and its content into the DOM
     parentDiv.insertBefore(newDiv, results[i]);
 }
