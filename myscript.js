bd94c9c66283dc96daabd9600d08033c15f984c3
Hong Yi Chen <hongyigma@gmail.com>
2018-02-03 16:12:26 -0500

    Made buttons on google

--- myscript.js
+++ myscript.js
@@ -1,13 +1,17 @@
 var results = document.getElementsByClassName("st");
 for (var i = 0; results.length; i++) {
     //Create the new content to be added
-    var newDiv = document.createElement("div"); 
-    var newContent = document.createTextNode("Hi there and greetings!"); 
-    
+    var newDiv = document.createElement("div");
+    var newContent = document.createTextNode("Hi there and greetings!");
+    var btn = document.createElement("BUTTON");
+    var t = document.createTextNode("CLICK ME");
+    btn.appendChild(t);
+
     //Grab the parent div so we can insert within the same place
     var parentDiv = results[i].parentNode;
-    newDiv.appendChild(newContent);  
+    newDiv.appendChild(newContent);
+    newDiv.appendChild(btn);
     // add the newly created element and its content into the DOM
-    //nextSibiling helps put the code AFTER results[i]  
+    //nextSibiling helps put the code AFTER results[i]
     parentDiv.insertBefore(newDiv, results[i].nextSibling);
 }
